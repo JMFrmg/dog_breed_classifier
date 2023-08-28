7396babc50ec8b6223e47643052eb739b131638d
mco <mco@2crsi.com>
2023-08-28 14:22:33 +0200

    api : global configs.

---
+++ api/app/config.py
@@ -0,0 +1,26 @@
+import torch
+
+GLOBAL_CONFIG = {
+    "WEIGHTS_PATH": "/app/model/inference_model.pt",
+    "CLASS_MAP_PATH": "/app/model/reverse_class_map.pickle",
+    "USE_CUDA_IF_AVAILABLE": True,
+    "GPU_ID": 0
+}
+
+def get_config() -> dict:
+    """
+    Get config
+    :return: dict
+    """
+
+    config = GLOBAL_CONFIG.copy()
+
+    if config["USE_CUDA_IF_AVAILABLE"]:
+        config["DEVICE"] = torch.device("cuda:" + str(config["GPU_ID"]) if torch.cuda.is_available() else "cpu")
+    else:
+        config["DEVICE"] = torch.device("cpu")
+
+
+    return config
+
+CONFIG = get_config()
