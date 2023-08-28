9d8c5cf124188f120fa0aeca63b59a448f592c2d
mco <mco@2crsi.com>
2023-08-29 00:43:36 +0200

    heroku9

--- api/app/config.py
+++ api/app/config.py
@@ -1,8 +1,8 @@
 import torch
 
 GLOBAL_CONFIG = {
-    "WEIGHTS_PATH": "/app/model_files/inference_model.pt",
-    "CLASS_MAP_PATH": "/app/model_files/reverse_class_map.pickle",
+    "WEIGHTS_PATH": "/code/app/model_files/inference_model.pt",
+    "CLASS_MAP_PATH": "/code/app/model_files/reverse_class_map.pickle",
     "USE_CUDA_IF_AVAILABLE": True,
     "GPU_ID": 0
 }
