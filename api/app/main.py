10943b1a6be7bd53ec9e3378dd9e0f058f72967d
mco <mco@2crsi.com>
2023-08-28 14:25:33 +0200

    api : load mode on startup.

--- api/app/main.py
+++ api/app/main.py
@@ -32,3 +32,32 @@ if __name__ != "__main__":
     fastapi_logger.setLevel(gunicorn_logger.level)
 else:
     fastapi_logger.setLevel(logging.DEBUG)
+
+# Load model only once when app start
+@app.on_event("startup")
+async def startup_event():
+    """
+    Initialize FastAPI and add variables
+    """
+    # Load model and pretrained weights
+    model = get_model()
+    model.load_state_dict(torch.load(CONFIG["WEIGHTS_PATH"], map_location=CONFIG["DEVICE"]))
+    logger.info("Model and weights loaded.")
+
+    model.eval()
+    model.to(CONFIG["DEVICE"])
+
+    # Images preprocessing
+    transform = get_transform()
+
+    # Class id to class name dictionnary
+    with open(CONFIG["CLASS_MAP_PATH"], "rb") as f:
+        id_to_class = pickle.load(f)
+
+
+    # add model and preprocess tools too app state
+    app.package = {
+        "id_to_class": id_to_class,
+        "transform": transform,
+        "model": model
+    }
